bfb1b338459ec846ff91b41b3447ec5c08d799b9
felipeprests <felipe.adriano@univille.br>
2018-09-09 22:02:02 -0300

    Criando a classe Page

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php 
-use Hcode\DB\Sql;
-
-
 require_once("vendor/autoload.php");
+use Hcode\DB\Sql;
+use \Slim\Slim;
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+
     
-	$sql = new Sql();
+	$page = new Page();
 
-	$result = $sql->select('SELECT * FROM tb_users');
-
-	echo json_encode($result);
+	$page->setTpl("index");
 
 });
 
---
+++ views-cache/header.d6353052c054bc5ba6b6a4bbc8bd866b.rtpl.php
@@ -0,0 +1,9 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Creative Store</title>
+</head>
+<body>
+    
