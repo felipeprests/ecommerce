57c5a2a59fb6ddba007b891e4f94c809273fabb5
felipeprests <felipe.adriano@univille.br>
2019-02-03 12:38:03 -0200

    autoload do hcodebr classes

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once("vendor/autoload.php");
 use Hcode\DB\Sql;
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 
 
@@ -10,11 +11,23 @@ $app = new Slim();
 
 $app->config('debug', true);
 
+
+//ROTAS
 $app->get('/', function() {
-
     
-	$page = new Page();
+	$sql = new Hcode\DB\Sql();
 
+	$results = $sql->select('SELECT * FROM tb_users');
+
+	echo json_encode($results);
+	// $page = new Page();
+	// $page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 });
